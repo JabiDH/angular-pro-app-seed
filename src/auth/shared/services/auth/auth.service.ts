739279641108cde2499f30fff3326e9a36972705
DESKTOP-9QU409Q\jabih <jabi.habash@gmail.com>
2019-11-11 17:01:22 -0500

    Implementing app-header and app-nav components

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { emit } from 'cluster';
+import { Store } from 'store'
+import 'rxjs/add/operator/do'
+
+export interface User{
+    email: string,
+    uid: string,
+    authenticated: boolean    
+}
 
 @Injectable()
 export class AuthService {
+
+    auth$ = this.af.authState
+        .do(next => {
+            if(!next){
+                this.strore.set('user', null);
+                return
+            }
+            const user: User = {
+                email: next.email,
+                uid: next.uid,
+                authenticated: true
+            };
+            this.strore.set('user', user);
+        });
+
     constructor(
-        private af: AngularFireAuth
+        private af: AngularFireAuth, 
+        private strore: Store
     ) { }
 
     createUser(email: string, password: string) {
@@ -16,6 +39,10 @@ export class AuthService {
     loginUser(email: string, password: string) {
         return this.af.auth
             .signInWithEmailAndPassword(email, password);
+    }
+
+    logoutUser(){
+        return this.af.auth.signOut();
     }
 
 }
